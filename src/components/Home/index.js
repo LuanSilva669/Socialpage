8288686213cfebc3412047e0d3bfb2ed13f53c9d
LuanSilva669 <125572605+LuanSilva669@users.noreply.github.com>
2023-05-31 17:59:04 -0300

    finish?

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,105 +1,207 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
+import React, { useState } from "react";
+import { FaGithub, FaTwitter, FaHeart, FaRetweet, FaComment } from "react-icons/fa";
+import Image from "next/image";
 
-// Importe sua logo e atribua um nome significativo ao componente
-import CustomLogo from '/hlogo.png';
-
-export default function Home() {
-  const [sidebar1Open, setSidebar1Open] = useState(false);
-  const [sidebar2Open, setSidebar2Open] = useState(false);
+const HomePage = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [tweetText, setTweetText] = useState("");
   const [tweets, setTweets] = useState([]);
-  const [newTweet, setNewTweet] = useState('');
 
-  const toggleSidebar1 = () => {
-    setSidebar1Open(!sidebar1Open);
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
   };
 
-  const toggleSidebar2 = () => {
-    setSidebar2Open(!sidebar2Open);
-  };
-
-  const handleNewTweetChange = (e) => {
-    setNewTweet(e.target.value);
-  };
-
-  const handleAddTweet = () => {
-    if (newTweet.trim() !== '') {
-      setTweets([...tweets, newTweet]);
-      setNewTweet('');
+  const handleTweetSubmit = (e) => {
+    e.preventDefault();
+    if (tweetText.trim() !== "") {
+      const newTweet = {
+        id: new Date().getTime(),
+        text: tweetText,
+        likes: 0,
+      };
+      setTweets([newTweet, ...tweets]);
+      setTweetText("");
     }
   };
 
+  const handleLikeClick = (tweetId) => {
+    const updatedTweets = tweets.map((tweet) => {
+      if (tweet.id === tweetId) {
+        return { ...tweet, likes: tweet.likes + 1 };
+      }
+      return tweet;
+    });
+    setTweets(updatedTweets);
+  };
+
+  const handleRetweetClick = (tweetId) => {
+    // handle retweet logic
+  };
+
+  const handleCommentClick = (tweetId) => {
+    // handle comment logic
+  };
+
   return (
-    <div className="bg-black min-h-screen">
-      <Head>
-        <title>Home</title>
-      </Head>
-
-      <div className="flex">
-        <div
-          id="sidebar1"
-          className={`w-1/4 bg-gray-200 ${
-            sidebar1Open ? 'translate-x-0' : '-translate-x-full'
-          } transition-transform duration-300 ease-in-out`}
-        >
-          <button
-            className="p-2 bg-blue-500 text-white rounded-md"
-            onClick={toggleSidebar1}
-          >
-            Toggle Sidebar 1
-          </button>
-        </div>
-
-        <div className="flex-1 p-5">
-          <div className="flex items-center mb-4">
-            {/* Substitua FiTwitter pelo seu componente de logo personalizado */}
-            <CustomLogo className="text-3xl text-blue-500 mr-2" />
-            <h1 className="text-3xl font-bold">Home</h1>
+    <div className="bg-fire min-h-screen flex flex-col text-white">
+      <title>Dark Society</title>
+      <header className="bg-black py-1">
+        <div className="container flex justify-between items-center mx-auto relative">
+          <div className="flex items-center">
+            <Image src="/hlogo.png" alt="Logo" width={60} height={60} />
           </div>
-          <div className="flex space-x-4 mb-4">
-            <div className="w-10 h-10 rounded-full bg-gray-300"></div>
-            <div className="flex-1">
-              <input
-                type="text"
-                placeholder="What's happening?"
-                className="w-full p-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-                value={newTweet}
-                onChange={handleNewTweetChange}
-              />
-              <button
-                className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
-                onClick={handleAddTweet}
+          <button
+            className="text-white focus:outline-none flex item"
+            onClick={handleMenuClick}
+          >
+            {!showMenu ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-6 w-6"
               >
-                Add Tweet
-              </button>
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 0 1 0-2h14a1 1 0 0 1 0 2H3zm0 6a1 1 0 0 1 0-2h14a1 1 0 0 1 0 2H3zm0 6a1 1 0 0 1 0-2h14a1 1 0 0 1 0 2H3z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 7.293a1 1 0 0 1 0-1.414l4-4a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1-1.414 1.414L11 5.414V17a1 1 0 1 1-2 0V5.414L4.707 7.707a
+                 1 1 0 0 1-1.414 0z"
+                />
+              </svg>
+            )}
+          </button>
+          {showMenu && (
+            <nav className="bg-black py-5 px-10 absolute top-full right-1 flex flex-col items-center">
+              <ul className="container flex flex-col items-center mx-auto">
+                <li className="my-2">
+                  <a
+                    href="https://github.com/LuanSilva669"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white hover:text-gray-200 transition-colors duration-300 py-2 px-4 rounded flex items-center"
+                  >
+                    <FaGithub className="mr-2" />
+                    GitHub
+                  </a>
+                </li>
+                <li className="my-2">
+                  <a
+                    href="https://twitter.com/marlborao666"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white hover:text-gray-200 transition-colors duration-300 py-2 px-4 rounded flex items-center"
+                  >
+                    <FaTwitter className="mr-2" />
+                    Twitter
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          )}
+        </div>
+      </header>
+
+      <div className="flex-grow flex justify-center items-center w-full">
+        <div className="bg-black text-white w-11/12 p-8 rounded-lg shadow-lg font-space-mono">
+          <div className="flex items-center mb-6">
+            <Image
+              src="/profile.png"
+              alt="Profile Picture"
+              width={60}
+              height={60}
+              className="rounded-full mr-4"
+            />
+            <div>
+              <h2 className="text-2xl font-bold">John Doe</h2>
+              <p className="text-gray-400">@johndoe</p>
             </div>
           </div>
-          <ul className="space-y-4">
-            {tweets.map((tweet, index) => (
-              <li
-                key={index}
-                className="p-4 border border-gray-300 rounded-xl text-white break-words"
-              >
-                {tweet}
-              </li>
+          <form className="flex flex-col mb-6" onSubmit={handleTweetSubmit}>
+            <textarea
+              className="border text-black border-gray-300 rounded-md px-4 py-3 mb-4 w-full resize-none"
+              rows="3"
+              placeholder="What's happening?"
+              value={tweetText}
+              onChange={(e) => setTweetText(e.target.value)}
+            ></textarea>
+            {tweetText.length > 10 && (
+              <p className="text-gray-400 text-sm mb-2">
+                {140 - tweetText.length} characters remaining
+              </p>
+            )}
+            <button
+              type="submit"
+              className="bg-gray-500 hover:bg-red-500 text-white rounded-md px-4 py-2 transition-colors duration-300 ml-auto w-24"
+            >
+              Tweet
+            </button>
+          </form>
+          <div className="overflow-y-auto max-h-40 space-y-6 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent">
+            {tweets.map((tweet) => (
+              <div className="flex items-start space-x-4" key={tweet.id}>
+                <Image
+                  src="/profile.png"
+                  alt="Profile Picture"
+                  width={50}
+                  height={50}
+                  className="rounded-full"
+                />
+                <div>
+                  <h3 className="text-xl font-bold">John Doe</h3>
+                  <p className="text-gray-400">{tweet.text}</p>
+                  <div className="flex items-center mt-2">
+                  <button
+    className="text-gray-400 hover:text-red-500 transition-colors duration-300 focus:outline-none mr-20"
+    onClick={() => handleRetweetClick(tweet.id)}
+  >
+    <FaRetweet className="mr-1" />
+    {tweet.retweets}
+  </button>
+  <button
+    className="text-gray-400 hover:text-red-500 transition-colors duration-300 focus:outline-none mr-20"
+    onClick={() => handleCommentClick(tweet.id)}
+  >
+    <FaComment className="mr-1" />
+    {tweet.comments}
+  </button>
+  <button
+    className="text-gray-400 hover:text-red-500 transition-colors duration-300 focus:outline-none"
+    onClick={() => handleLikeClick(tweet.id)}
+  >
+    <FaHeart className="mr-" />
+   
+  </button>
+                  </div>
+                </div>
+              </div>
             ))}
-          </ul>
-        </div>
-
-        <div
-          id="sidebar2"
-          className={`w-1/4 bg-gray-200 ${
-            sidebar2Open ? 'translate-x-0' : 'translate-x-full'
-          } transition-transform duration-300 ease-in-out`}
-        >
-          <button
-            className="p-2 bg-blue-500 text-white rounded-md"
-            onClick={toggleSidebar2}
-          >
-            Toggle Sidebar 2
-          </button>
+          </div>
         </div>
       </div>
+
+      <footer className="bg-black py-1">
+      <div className="container flex justify-center mx-auto">
+          <FaGithub
+            className="text-3xl text-white cursor-pointer hover:text-gray-200"
+            onClick={() =>
+              window.open("https://github.com/LuanSilva669/Socialpage")
+            }
+          />
+        </div>
+      </footer>
     </div>
   );
-}
+};
+
+export default HomePage;
